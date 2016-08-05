ea99980a267d04ba443aab9d941ffe2d37d477c0
Joar Classon <joarc@joarc.se>
2016-08-05 22:34:29 +0200

    La till lite grundläggande filer

---
+++ api.js
@@ -0,0 +1,6 @@
+/*
+ * Grundämnen.se API modul
+ * Skapad av Joar Classon och Gustav Ekner
+ *
+ */
+// API Modul, flytta saker från editor/app.js#api-delen hit och gör lite andra bra grejer så api:n funkar bättre.
